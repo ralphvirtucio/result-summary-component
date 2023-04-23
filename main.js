d95ac95fe0f04c3b4ee729cddb8d4f6e5c9e4cb5
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-04-23 23:57:25 +0800

    WIP: Dynamic Content

--- main.js
+++ main.js
@@ -6,16 +6,51 @@ fetch('./data.json')
 
 const createNodes = (datas) => {
   datas.map((data) => {
-    // const summaryListItem = document.createElement('li');
-    // const summaryListItemContent = document.createElement('div');
-    // const summaryLogo = document.createElement('img');
-    // const summaryText = document.createElement('p');
-    // const summaryScore = document.createElement('p');
-    // const summaryUserScore = document.createElement('strong');
-    // summaryLogo.setAttribute('src', nodeData.icon);
-    // summaryText.textContent = nodeData.category;
-    // summaryListItemContent.append(summaryLogo);
-    // summaryListItemContent.appendChild(summaryText);
-    // console.log(summaryListItemContent);
+    const summaryListItem = document.createElement('li');
+    const summaryListItemContent = document.createElement('div');
+    const summaryLogo = document.createElement('img');
+    const summaryText = document.createElement('p');
+    const summaryScore = document.createElement('p');
+
+    summaryLogo.setAttribute('src', data.icon);
+    summaryText.textContent = data.category;
+
+    summaryListItemContent.append(summaryLogo);
+    summaryListItemContent.appendChild(summaryText);
+    summaryListItemContent.classList.add('summary__list-item--content');
+
+    let score = `<strong>${data.score}</strong>  / 100`;
+
+    summaryScore.insertAdjacentHTML('afterbegin', score);
+    summaryScore.style.color = 'hsl(224, 30%, 27%)';
+
+    summaryListItem.append(summaryListItemContent);
+    summaryListItem.appendChild(summaryScore);
+
+    summaryList.append(summaryListItem);
+
+    summaryListItem.classList.add('summary__list-item');
+
+    switch (data.category.toLowerCase()) {
+      case 'reaction':
+        summaryListItem.style.backgroundColor = 'hsl(0, 100%, 67%)';
+        summaryText.style.color = 'hsl(0, 100%, 67%)';
+        break;
+      case 'memory':
+        summaryListItem.style.backgroundColor = 'hsl(39, 100%, 56%)';
+        summaryText.style.color = 'hsl(39, 100%, 56%)';
+
+        break;
+      case 'verbal':
+        summaryListItem.style.backgroundColor = 'hsl(166, 100%, 37%)';
+        summaryText.style.color = 'hsl(166, 100%, 37%)';
+
+        break;
+      case 'visual':
+        summaryListItem.style.backgroundColor = 'hsl(234, 85%, 45%)';
+        summaryText.style.color = 'hsl(234, 85%, 45%)';
+        break;
+      default:
+    }
   });
 };
